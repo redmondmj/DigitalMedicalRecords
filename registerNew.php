1bba1873c06ca03b303125edf50c3ff8a47aa845
Robert LeCreux <robertlecreux@hotmail.com>
2017-10-05 14:59:03 -0300

    altered registerNew for nulls

--- registerNew.php
+++ registerNew.php
@@ -1,32 +1,66 @@
 <?php
-    require_once "db.php";    
+    require_once "db.php";
     
-    $firstName = test_input($_POST['fName']);
-    $lastName = test_input($_POST['lName']);
-    $village = test_input($_POST['village']);
-    $birth = test_input($_POST['dob']);
+    //Receive the RAW post data.
+    $content = trim(file_get_contents("php://input"));
+    
+    //Attempt to decode the incoming RAW post data from JSON.
+    $decoded = json_decode($content, true); 
+    
+    $firstName = $decoded[0]->first;
+    $lastName = $decoded[0]->last;
+    $village = $decoded[0]->village;
+    $birth = $decoded[0]->age;
+    
     if(!empty($_POST['gender-0'])){
         $sex = "male";
-    } else if (!empty($_POST['gender-1'])){
+    } elseif (!empty($_POST['gender-1'])){
         $sex = "female";
     }
-    
-    if (!empty($firstName) && !empty($lastName) && !empty($village) && !empty($birth) && !empty($sex)){
-    	$sql = "INSERT INTO patient (FirstName, LastName, Birthday, Sex, Village) VALUES (?,?,?,?,?)";
-        try{
-            $stmt = mysqli_prepare($connection, $sql);
-            mysqli_stmt_bind_param($stmt, $firstName, $lastName, $birth, $sex, $village);
-            $query = mysqli_query($connection, $stmt) or die(mysqli_error($connection));
-            if(mysqli_stmt_execute($connection, $sql)){
-                echo true;
-            }
-        } catch(Error $e){
-            echo false;
-        } finally {
-            mysqli_stmt_close();
-            mysqli_close();
-        }
+
+    $sql = "INSERT INTO patient (";
+    $values = ") VALUES (";
+    if (!empty($firstName)){
+        $sql .= "FirstName,";
+        $values .= "$firstName,";
     }
+    if (!empty($lastName)) {
+        $sql .= "LastName,";
+        $values .= "$lastName,";
+    }
+    if (!empty($village)) {
+        $sql .= "Village,";
+        $values .= "$village,";
+    }
+    if (!empty($birth)){
+        $sql .= "Birthday,";
+        $values .= "$birth,";
+    }
+    if (!empty($sex)){
+        $sql .= "Sex,";
+        $values .= "$sex,";
+    }
+
+    if(substr_compare($sql, ",", -1, 1)){
+        substr_replace($sql ,")",-1);
+        substr_replace($values ,")",-1);
+    }
+
+    $sql .= $values;
+    
+    try{
+        $stmt = mysqli_prepare($connection, $sql);
+        $query = mysqli_query($connection, $stmt) or die(mysqli_error($connection));
+        if(mysqli_stmt_execute($connection, $sql)){
+            echo true;
+        }
+    } catch(Error $e){
+        echo false;
+    } finally {
+        mysqli_stmt_close();
+        mysqli_close();
+    }
+    
 
     function test_input($data) {
 	    $data = trim($data);
