c5e0963bb1d81bcfc8d69976c8a01e060e914860
Matt Redmond <matt.redmond@nscc.ca>
2017-10-09 13:35:07 -0300

    Added jquery to each page before bootstrap.

--- pharma.php
+++ pharma.php
@@ -23,7 +23,8 @@
         }
     </script>
     <title>CACHA - Pharmacy</title>
-
+    <!-- load jQuery -->
+    <script src="bin/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap core CSS -->
     <link href="bin/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
